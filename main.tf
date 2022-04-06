b78e45634ab214d666b996e1ac9a6b94209d12bf
JoranSlingerland <32357871+JoranSlingerland@users.noreply.github.com>
2022-04-06 21:13:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
     }
   }
-    backend "azurerm" {
-        resource_group_name  = "tfstate"
-        storage_account_name = "tfstate908957484"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate908957484"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 
 }
 
 # variables
 variable "resource_group_name" {
-  type =  list
-  default =  [
-      {
-        "name" = "rg-mgmt-prod-westeu-001-tf"
-      },
-      {
-        "name" = "rg-network-prod-westeu-tf"
-      }
+  type = list(any)
+  default = [
+    {
+      "name" = "rg-mgmt-prod-westeu-001-tf"
+    },
+    {
+      "name" = "rg-network-prod-westeu-tf"
+    }
   ]
 }
 
@@ -31,7 +31,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg000" {
-  name = var.resource_group_name[0].name
+  name     = var.resource_group_name[0].name
   location = "westeurope"
   tags = {
     "env" = "prod"
@@ -39,7 +39,7 @@ resource "azurerm_resource_group" "rg000" {
 }
 
 resource "azurerm_resource_group" "rg001" {
-  name = var.resource_group_name[1].name
+  name     = var.resource_group_name[1].name
   location = "westeurope"
   tags = {
     "env" = "prod"
